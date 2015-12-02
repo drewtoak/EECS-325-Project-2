20dcf346e58e962ee4e189e4f50990a0c7c4c80d
drewtoak <ath50@case.edu>
2015-12-01 21:52:29 -0500

    made few changes in distMeasurement

--- geoDistance.py
+++ geoDistance.py
@@ -3,11 +3,11 @@ from socket import gethostbyname
 from math import *
 
 def main(dest_name):
-    location = get_my_location(gethostbyname(dest_name))
+    location = locate(gethostbyname(dest_name))
 
-def get_my_location(IP_address):
+def locate(IP_address):
     response = urllib2.urlopen("freegeoip.net/xml/{}".format(IP_address)).read
-    print response
+    
 
 if __name__ == "__main__":
     main("google.com")
