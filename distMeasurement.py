9d147ecebc7deea3855e4c0348b76791aaebd7a6
drewtoak <ath50@case.edu>
2015-12-01 20:06:27 -0500

    small edit

--- distMeasurement.py
+++ distMeasurement.py
@@ -31,7 +31,7 @@ def main(host_name):
     # output_file.close()
 
     result = probe(gethostbyname(host_name))
-    if result[0] is not None:
+    if result[0] is not 0:
         output_result = "Host: {}\nHops: {}\nRTT: {} ms\n".format(host_name, result[0], result[1])
         print output_result
     else:
